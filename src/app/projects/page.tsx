0132706cf390cb681275ce439dc1464cb4e62eb0
Robbie Gollan <robbiegollan@gmail.com>
2023-11-23 15:49:36 +1100

    refines projects page, removes some disclaimers

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -19,7 +19,6 @@ export interface ProjectsData {
 	imageLinks: string[];
 	projectType: string;
 	githubLink: string;
-	status: string;
 	liveLink?: string;
 }
 
@@ -50,22 +49,17 @@ const ProjectsPage = () => {
 		<>
 			<Header />
 			<ContentWrapper>
-				<p className='text-xs text-brand-accent text-center'>
-					Site still being built but deployed to give preliminary details of
-					myself and my work. Best accessed on a computer/tablet. Small screens
-					not fully supported. (23rd November, 2023)
-				</p>
 				<section className=' text-brand-body'>
 					<h1 className='text-center text-5xl text-brand-heading font-bold py-4'>
 						My Projects
 					</h1>
-					<h2 className='text-2xl text-brand-heading mb-4 font-bold'>
+					{/* <h2 className='text-2xl text-brand-heading mb-4 font-bold'>
 						Featured Projects
-					</h2>
+					</h2> */}
 					<FeaturedProjects projects={featuredProjects} />
-					<h2 className='text-2xl text-brand-heading mb-4 font-bold'>
+					{/* <h2 className='text-2xl text-brand-heading mb-4 font-bold'>
 						Other Projects
-					</h2>
+					</h2> */}
 					<UnfeaturedProjects projects={unfeaturedProjects} />
 				</section>
 			</ContentWrapper>
